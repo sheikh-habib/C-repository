d443b7f7716c0c5d10243bf75df9f32549d86b43
sheikh-habib <habib150775@gmail.com>
2025-12-17 13:44:41 +0500

    Add RFQ model with properties

---
+++ Models/RFQ.cs
@@ -0,0 +1,8 @@
+public class RFQ
+{
+    public int Id { get; set; }
+    public string BuyerName { get; set; }
+    public string Product { get; set; }
+    public int Quantity { get; set; }
+    public string Message { get; set; }
+}
